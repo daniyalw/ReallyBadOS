65e95097130fab308f70b0dd35b27d1e994f3094
Daniyal-Warraich <84168421+Daniyal-Warraich@users.noreply.github.com>
2021-10-07 23:07:02 -0600

    fixes

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -17,23 +17,21 @@ int height = 200;
 #include "kernel/outw.cpp"
 #include "kernel/cursor.cpp"
 #include "kernel/shutdown.cpp"
+/*
 #include "kernel/putpixel.cpp"
-
+*/
 #include "include/string.cpp"
 
 // drivers
 // very bad code ahead
+/*
 #include "drivers/video/clear.cpp"
 #include "drivers/keyboard.cpp"
 #include "drivers/mouse.cpp"
 
-
 #include "include/printf.cpp"
+*/
 
 extern "C" void kmain()
 {
-	short * loc = (short *)0xb8000;
-	// get century
-	outb(0x70, 0x09);
-	unsigned char h = inb(0x71);
 }
--- src/kernel/system.h
+++ src/kernel/system.h
@@ -1,7 +1,4 @@
-// functions
-int system(char * command);
-
 // variables
-char * platform = "CeneOS Pro";
-char * author = "Daniyal Warraich";
-char * version = "0.1";
+#define platform CeneOS
+#define author Daniyal Warraich
+#define version 0.1
