2390b6b2651f94bb2d64e80c7ba787e36c996947
Daniyal-Warraich <84168421+Daniyal-Warraich@users.noreply.github.com>
2021-10-08 16:22:47 -0600

    stdint.h improved

--- src/kernel/stdint.h
+++ src/kernel/stdint.h
@@ -1,2 +1,15 @@
 #define true 1
 #define false 0
+
+typedef unsigned int   u32;
+typedef          int   s32;
+typedef unsigned short u16;
+typedef          short s16;
+typedef unsigned char  u8;
+typedef          char  s8;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#define low_16(address) (u16)((address) & 0xFFFF)
+#define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
