1c3ace275066b6fd01d074b8d8a11673aa59ef23
Daniyal-Warraich <84168421+Daniyal-Warraich@users.noreply.github.com>
2022-03-15 20:50:23 -0600

    [disk] rewrite fs

--- kernel/diskfs.cpp
+++ kernel/diskfs.cpp
@@ -1,254 +1,296 @@
 #include "diskfs.h"
 
-// disk_write_function should have 3 parameters of sector, sector_count, uint8_t*
-void format()
+int format()
 {
-    disk_fs_master_t *master = (disk_fs_master_t *)malloc(sizeof(disk_fs_master_t *));
+    uint8_t *bytes;
+    bytes = disk->read(bytes, FS_BEGINNING_SECTOR, 1);
+
+    disk_master_t *tmp_master = (disk_master_t *)bytes;
+
+    if (tmp_master->magic == FS_MAGIC)
+    {
+        log::warning("diskfs: disk %d already formatted with filesystem", selected_disk);
+        return 1;
+    }
+
+    disk_master_t *master = (disk_master_t *)malloc(sizeof(disk_master_t));
+
+    if (master == NULL)
+    {
+        log::warning("diskfs: Not enough space for disk_master_t*");
+        return 1;
+    }
 
     master->magic = FS_MAGIC;
     master->state = FS_NOT_NULL;
-    master->free_sector = 4;
+    master->free_sector = FS_BEGINNING_SECTOR + 1;
     master->files = 0;
 
-    disk_write(FS_BEGINNING_SECTOR, 1, (uint8_t *)master);
+    disk->write(FS_BEGINNING_SECTOR, (uint8_t *)master);
 
-    free(master);
-
-    return;
-}
-
-disk_fs_master_t *get_master()
-{
-    uint8_t *bytes;
-    bytes = disk_read(bytes, FS_BEGINNING_SECTOR, 1);
-    disk_fs_master_t *master = (disk_fs_master_t *)bytes;
-    return master;
-}
-
-// uint8_t *disk_read_function(bytes, lba, sectors);
-// disk write same as format
-int write_new_file_disk(char *name, char *path, char *contents, int type)
-{
-    disk_fs_master_t *master = get_master();
-    int contents_sectors = strlen(contents)/512 + 1;
-
-    disk_fs_node_t *node = (disk_fs_node_t *)malloc(sizeof(disk_fs_node_t));
-
-    strcpy(node->name, name);
-    strcpy(node->path, path);
-    node->type = type;
-    node->magic = FS_MAGIC;
-    node->state = FS_NOT_NULL;
-    node->contents_sectors = contents_sectors;
-
-    node->offset = master->free_sector;
-
-    disk_write(master->free_sector, 1, (uint8_t *)node);
-    disk_write(master->free_sector + 1, contents_sectors, (uint8_t *)contents);
-
-    master->free_sector++;
-    master->free_sector += contents_sectors;
-    master->files++;
-
-    disk_write(FS_BEGINNING_SECTOR, 1, (uint8_t *)master);
-
-    free(node);
+    free(master); // already wrote to disk so we don't need it
 
     return 0;
 }
 
-void edit_disk_file(disk_fs_node_t *node, char *new_contents)
+int disk_write(char *fname, char *fparent, char *fpath, int type, int id, char *contents)
 {
-    int contents_sectors = strlen(new_contents)/512 + 1;
-    int old_sectors = node->contents_sectors;
+    uint8_t *bytes;
+    bytes = disk->read(bytes, FS_BEGINNING_SECTOR, 1);
 
-    if (contents_sectors > old_sectors)
-    {
-        move_sectors_up(node->offset + old_sectors + 1, contents_sectors - old_sectors);
-        node->contents_sectors = contents_sectors;
-
-        disk_write(node->offset, 1, (uint8_t *)node);
-        disk_write(node->offset + 1, contents_sectors, (uint8_t *)new_contents);
-    }
-    else if (contents_sectors < old_sectors)
-    {
-        move_sectors_down(node->offset + 1 + old_sectors, old_sectors - contents_sectors);
-        node->contents_sectors = contents_sectors;
-
-        disk_write(node->offset, 1, (uint8_t *)node);
-        disk_write(node->offset + 1, contents_sectors, (uint8_t *)new_contents);
-    }
-    else
-    {
-        printf("a");
-        putchar('\b', 0x0F00);
-        disk_write(node->offset + 1, old_sectors, (uint8_t *)new_contents);
-    }
-}
-
-void edit_disk_file(char *path, char *new_contents)
-{
-    disk_fs_node_t *node = disk_file_open(path);
-    edit_disk_file(node, new_contents);
-}
-
-void move_sector_up(int beginning_sector)
-{
-    disk_fs_master_t *master = get_master();
-
-    int top = master->free_sector - 1;
-
-    for (int z = top; z >= beginning_sector; z--)
-    {
-        uint8_t *bytes;
-        bytes = disk_read(bytes, z, 1);
-
-        disk_fs_node_t *node = (disk_fs_node_t *)bytes;
-
-        if (node->magic == FS_MAGIC)
-        {
-            node->offset++;
-            disk_write(z + 1, 1, (uint8_t *)node);
-        }
-        else
-        {
-            disk_write(z + 1, 1, bytes);
-        }
-    }
-}
-
-void move_sectors_up(int beginning_sector, int sectors)
-{
-    for (int z = 0; z < sectors; z++)
-    {
-        move_sector_up(beginning_sector);
-        beginning_sector++;
-    }
-}
-
-// sector parameter is the sector that is no longer needed
-void move_sector_down(int sector)
-{
-    disk_fs_master_t *master = get_master();
-
-    int top = master->free_sector;
-
-    for (int z = sector; z < top; z++)
-    {
-        uint8_t *bytes;
-        bytes = disk_read(bytes, z + 1, 1);
-
-        disk_fs_node_t *node = (disk_fs_node_t *)bytes;
-
-        if (node->magic == FS_MAGIC)
-        {
-            node->offset--;
-            disk_write(z, 1, (uint8_t *)node);
-        }
-        else
-        {
-            disk_write(z, 1, bytes);
-        }
-    }
-}
-
-void move_sectors_down(int sector, int sectors)
-{
-    for (int z = sector + sectors; z >= sector; z--)
-        move_sector_down(z);
-}
-
-int find_offset(disk_fs_node_t *node)
-{
-    return node->offset;
-}
-
-int find_sector(disk_fs_node_t *node)
-{
-    return node->offset;
-}
-
-// sectors start at 0
-char *read_file_sectors(disk_fs_node_t *node, int sector, int sectors_to_read, char *buf, int bufsize)
-{
-    uint8_t *bytes = (uint8_t *)malloc(512 * sizeof(uint8_t));
-    disk_read(bytes, node->offset, 1);
-
-    disk_fs_node_t *_node = (disk_fs_node_t *)bytes;
-
-    bytes = disk_read(bytes, node->offset + sector + 1, sectors_to_read);
-
-    for (int z = 0; z < bufsize; z++)
-        buf[z] = (char)bytes[z];
-
-    free(bytes);
-
-    return buf;
-}
-
-char *read_file_sectors(disk_fs_node_t *node, char *buf)
-{
-    return read_file_sectors(node, 0, node->contents_sectors, buf, node->contents_sectors * 512);
-}
-
-disk_fs_node_t *disk_file_open(char *path)
-{
-    disk_fs_master_t *master = get_master();
+    disk_master_t *master = (disk_master_t *)bytes;
 
     if (master->magic != FS_MAGIC)
     {
-        printf("Master sector not FS_MAGIC!\n");
+        log::warning("diskfs: invalid magic");
+        return 1;
+    }
+
+    disk_node_t *node = (disk_node_t *)malloc(sizeof(disk_node_t));
+
+    if (node == NULL)
+    {
+        log::warning("diskfs: not enough space for disk_node_t*");
+        return 1;
+    }
+
+    int sectors = strlen(contents)/512 + 1;
+
+    memset(node->name, 0, 20);
+    memset(node->path, 0, 100);
+    memset(node->parent, 0, 100);
+
+    strcpy(node->name, fname);
+    strcpy(node->path, fpath);
+    strcpy(node->parent, fparent);
+
+    node->type = type;
+    node->magic = FS_MAGIC;
+    node->state = FS_NOT_NULL;
+    node->contents_sectors = sectors;
+    node->id = id;
+
+    disk->write(master->free_sector, (uint8_t *)node);
+
+    master->free_sector + 1;
+    master->free_sector += sectors;
+
+    disk->write(FS_BEGINNING_SECTOR, (uint8_t *)master);
+
+    disk->write(master->free_sector - sectors, (uint8_t *)contents);
+
+    return 0;
+}
+
+disk_node_t *disk_find_node(uint8_t *bytes, char *path)
+{
+    bytes = disk->read(bytes, FS_BEGINNING_SECTOR, 1);
+
+    disk_master_t *master = (disk_master_t *)bytes;
+
+    if (master->magic != FS_MAGIC)
+    {
+        log::warning("diskfs: invalid master magic");
+        return NULL;
+    }
+
+    int top = master->free_sector;
+    int reach = 0;
+
+    for (int sector = FS_BEGINNING_SECTOR; sector < top; sector++)
+    {
+        if (reach == 0)
+        {
+            bytes = disk->read(bytes, sector, 1);
+            disk_node_t *node = (disk_node_t *)bytes;
+
+            if (node->magic != FS_MAGIC)
+            {
+                log::warning("diskfs: invalid node magic (a)");
+                return NULL;
+            }
+
+            if (strcmp(node->path, path) == 0)
+                return node;
+
+            reach = node->contents_sectors;
+        }
+        else
+        {
+            reach--;
+        }
+    }
+
+    return NULL;
+}
+
+disk_node_t *disk_open(char *path)
+{
+    uint8_t *bytes;
+    return disk_find_node(bytes, path);
+}
+
+int disk_sector_down(int sector)
+{
+    uint8_t *bytes;
+    bytes = disk->read(bytes, FS_BEGINNING_SECTOR, 1);
+
+    disk_master_t *master = (disk_master_t *)bytes;
+
+    if (master->magic != FS_MAGIC)
+    {
+        log::warning("diskfs: invalid master magic");
         return NULL;
     }
 
     int top = master->free_sector;
 
-    for (int z = FS_BEGINNING_SECTOR + 1; z < top; z++)
+    for (int z = top; z > sector; z--)
     {
-        uint8_t *bytes;
-        bytes = disk_read(bytes, z, 1);
-
-        disk_fs_node_t *node = (disk_fs_node_t *)bytes;
-
-        if (node->magic == FS_MAGIC && strcmp(node->path, path) == 0)
-            return node;
+        bytes = disk->read(bytes, z, 1);
+        disk->write(z - 1, bytes);
     }
 
+    master->free_sector--;
 
-    return NULL;
+    disk->write(FS_BEGINNING_SECTOR, (uint8_t *)master);
+
+    return 0;
+}
+
+int disk_sector_up(int sector)
+{
+    uint8_t *bytes;
+    bytes = disk->read(bytes, FS_BEGINNING_SECTOR, 1);
+
+    disk_master_t *master = (disk_master_t *)bytes;
+
+    if (master->magic != FS_MAGIC)
+    {
+        log::warning("diskfs: invalid master magic");
+        return NULL;
+    }
+
+    int top = master->free_sector;
+
+    for (int z = top; z >= sector; z--)
+    {
+        bytes = disk->read(bytes, z, 1);
+        disk->write(z + 1, bytes);
+    }
+
+    return 0;
+}
+
+void disk_up_sectors(int sector, int sectors)
+{
+    for (int z = 0; z < sectors; z++)
+        disk_sector_up(sector + z);
+}
+
+void disk_down_sectors(int sector, int sectors)
+{
+    for (int z = sectors; z >= 0; z--)
+        disk_sector_down(sector + z);
+}
+
+// offset is beginning sector of contents
+void edit_file_size(disk_node_t *node, int offset, char *nc)
+{
+    int sectors = strlen(nc)/512 + 1;
+
+    if (sectors < node->contents_sectors)
+        disk_down_sectors(offset + (node->contents_sectors - sectors), node->contents_sectors - sectors);
+    else if (sectors > node->contents_sectors)
+        disk_up_sectors(offset + node->contents_sectors, sectors - node->contents_sectors);
+
+    disk->write(offset, (uint8_t *)nc);
+}
+
+int disk_find_sector(disk_node_t *node)
+{
+    uint8_t *bytes;
+    bytes = disk->read(bytes, FS_BEGINNING_SECTOR, 1);
+
+    disk_master_t *master = (disk_master_t *)bytes;
+
+    if (master->magic != FS_MAGIC)
+    {
+        log::warning("diskfs: invalid master magic");
+        return NULL;
+    }
+
+    int top = master->free_sector;
+    int reached = 0;
+
+    for (int z = FS_BEGINNING_SECTOR + 1; z < top; z++)
+    {
+        if (reached == 0)
+        {
+            bytes = disk->read(bytes, z, 1);
+            disk_node_t *_node = (disk_node_t *)bytes;
+
+            if (_node->magic != FS_MAGIC)
+            {
+                log::warning("diskfs: invalid node magic");
+                return -1;
+            }
+
+            if (strcmp(_node->path, node->path) == 0)
+                return z;
+
+            reached = _node->contents_sectors;
+        }
+        else
+        {
+            reached--;
+        }
+    }
+
+    return -1;
+}
+
+int disk_find_sector(char *path)
+{
+    disk_node_t *node = disk_open(path);
+
+    if (node == NULL)
+        return -1;
+
+    return disk_find_sector(node);
+}
+
+char *disk_read(disk_node_t *node, int size, char *buf)
+{
+    int sector = disk_find_sector(node);
+
+    if (sector == -1)
+        return NULL;
+
+    uint8_t *bytes;
+    bytes = disk->read(bytes, sector + 1, size/512 + 1);
+
+    memset(buf, 0, size + 1);
+
+    for (int z = 0; z < size; z++)
+        buf[z] = (char)bytes[z];
+
+    return buf;
 }
 
 int write_vfs_disk(fs_node_t *node, int offset, int size, char *buf)
 {
-    char target[100];
-    sscanf(node->path, "/disk0%s", &target);
-    disk_fs_node_t *_node = disk_file_open(target);
-
-    if (_node == NULL)
-        return 1;
-
-    edit_disk_file(_node, buf);
-
     return 0;
 }
 
 int read_vfs_disk(fs_node_t *node, int offset, int size, char *buf)
 {
-    disk_fs_node_t *_node = disk_file_open("/hello.txt");
-
-    if (_node == NULL)
-    {
-        printf("_node is null.\n");
-        return NULL;
-    }
-
-    read_file_sectors(_node, 0, 1, buf, size);
     return 0;
 }
 
 int true_mkfile_vfs_disk(fs_node_t *node, __read read, __write write)
 {
-    return write_new_file_disk(node->name, node->path, "", 1);
+    return 0;
 }
 
 int tmp_mkfile_vfs_disk(fs_node_t *node, __read read, __write write)
@@ -258,42 +300,11 @@ int tmp_mkfile_vfs_disk(fs_node_t *node, __read read, __write write)
 
 void disk_init(fs_node_t *disk0)
 {
-    printf("b");
-    disk_write(0, 1, (uint8_t *)"fuck");
-    // if we don't do this, the fucking thing will just division-by-zero fault
-    printf("a");
+    disk = get_disk(selected_disk);
 
-    if (disk0 == NULL)
-    {
-        printf("Failed to mount disk.\n");
-        return;
-    }
 
-    disk_fs_master_t *master = get_master();
-
-    if (master->magic != FS_MAGIC)
-    {
-        printf("Master magic != FS magic!\n");
-        return;
-    }
-
-    int sector = FS_BEGINNING_SECTOR + 1;
-
-    while (true)
-    {
-        if (sector >= master->free_sector)
-            break;
-
-        uint8_t *bytes;
-        bytes = disk_read(bytes, sector, 1);
-        disk_fs_node_t *node = (disk_fs_node_t *)bytes;
-        printf("File found: %s\n", node->path);
-
-        sector += 1 + node->contents_sectors;
-
-        create_file(node->name, "/disk0/");
-    }
-
-    disk0->mkfile = true_mkfile_vfs_disk;
-    nodes[disk0->id] = disk0;
+    if (disk == NULL)
+        log::error("diskfs: Disk %d not found.", selected_disk);
+    else
+        log::info("diskfs: disk %d successfully initialized!", selected_disk);
 }
--- kernel/diskfs.h
+++ kernel/diskfs.h
@@ -20,43 +20,21 @@ typedef struct
     int files;
 
     char padding[508];
-} disk_fs_master_t;
+} __attribute__((packed)) disk_master_t;
 
 typedef struct
 {
     char name[20];
     char path[100];
+    char parent[100];
     int type; // type of node
     int magic; // again, always should be FS_MAGIC
     int state;
+    int id;
     int contents_sectors; // how many sectors does the contents take up
 
-    int offset = 0;
+    char padding[287];
+} __attribute__((packed)) disk_node_t;
 
-    char padding[387];
-} disk_fs_node_t;
-
-typedef uint8_t* (*disk_read_t)(uint8_t*, uint32_t, uint8_t);
-typedef void (*disk_write_t)(uint32_t, uint8_t, uint8_t*);
-
-disk_read_t disk_read = ata_read;
-disk_write_t disk_write = ata_write;
-
-void format();
-disk_fs_master_t *get_master();
-
-int write_new_file_disk(char *name, char *path, char *contents, int type);
-
-void edit_disk_file(disk_fs_node_t *node, char *new_contents);
-void edit_disk_file(char *path, char *new_contents);
-
-void move_sector_up(int beginning_sector);
-void move_sectors_up(int beginning_sector, int sectors);
-void move_sector_down(int sector);
-void move_sectors_down(int sector, int sectors);
-
-int find_offset(disk_fs_node_t *node);
-int find_sector(disk_fs_node_t *node);
-
-char *read_file_sectors(disk_fs_node_t *node, int sector, int sectors_to_read, char *buf, int bufsize);
-disk_fs_node_t *disk_file_open(char *path);
+disk_t *disk;
+int selected_disk = 0;
