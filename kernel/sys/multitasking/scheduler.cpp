8d843c8a7a1530e35e236ee3db20732175479790
Daniyal-Warraich <84168421+Daniyal-Warraich@users.noreply.github.com>
2022-05-08 22:16:37 -0600

    [multitasking] replace onerous namespace names with using

--- kernel/sys/multitasking/scheduler.cpp
+++ kernel/sys/multitasking/scheduler.cpp
@@ -1,26 +1,29 @@
 #include "scheduler.h"
 #include "task.h"
 
+using namespace Kernel;
+using namespace CPU;
+
 namespace Kernel {
 
     namespace CPU {
 
         pid_t schedule() {
             // simple round-robin scheduler
-            Kernel::CPU::current_task++;
+            current_task++;
 
-            if (Kernel::CPU::current_task >= Kernel::CPU::task_count)
-                Kernel::CPU::current_task = 0;
+            if (current_task >= task_count)
+                current_task = 0;
 
-            while (Kernel::CPU::tasks[Kernel::CPU::current_task]->null || Kernel::CPU::tasks[Kernel::CPU::current_task]->blocked)
+            while (tasks[current_task]->null || tasks[current_task]->blocked)
             {
-                Kernel::CPU::current_task++;
+                current_task++;
 
-                if (Kernel::CPU::current_task >= Kernel::CPU::task_count)
-                    Kernel::CPU::current_task = 0;
+                if (current_task >= task_count)
+                    current_task = 0;
             }
 
-            return Kernel::CPU::current_task;
+            return current_task;
         }
 
     }
